0d487001314b1260cd3061a8ddbfdc54ef37994a
Yaroviy Vitaliy <vitalya.yaroviy97@gmail.com>
2018-01-24 17:49:20 +0200

    Second commit - fixed bags

--- index.js
+++ index.js
@@ -1,87 +1,81 @@
-let div = document.querySelector('div');
+let main = document.querySelector('div');
 let img = document.querySelector('img');
 
-div.style.width = img.offsetWidth + 'px';
-div.className = 'main';
+main.style.width = img.offsetWidth + 'px';
+main.style.height = img.offsetHeight + 'px';
+main.className = 'main';
+main.id = 'id';
 
 let input = document.querySelector('input');
 
 
 function addDiv (e) {
+
     let container = document.createElement('div');
-    container.className = 'flex';
+
+    container.className = 'container';
+
     let childDiv = document.createElement('div');
+
+
     childDiv.className = 'childDiv';
+
     let x = document.createElement('div');
+
     x.innerText = ' X';
+
     x.className = 'close';
+
     x.style.display = 'none';
 
     if(input.value !== ''){
-        let val = input.value;
-        container.style.top = e.y + 'px';
-        container.style.left = e.x + 'px';
-        childDiv.innerText = val;
-        let imageParam = div.getBoundingClientRect();
-        container.style.left = (e.x - imageParam.left - imageParam.width / 16)  + 'px';
-        container.style.top = (e.y - imageParam.top - imageParam.height) + 'px';
-
+        childDiv.innerText = input.value;
         input.value = '';
+
+        container.style.top = e.y - main.offsetTop + 'px';
+        container.style.left = e.x - main.offsetLeft - 25 + 'px';
+
+        childDiv.addEventListener('click', close);
+
+        x.addEventListener('click', removeDiv);
+
         container.appendChild(childDiv);
         container.appendChild(x);
-        childDiv.addEventListener('click', close);
-        x.addEventListener('click', removeDiv);
-        div.appendChild(container);
+        main.appendChild(container);
     }
 }
 
-img.addEventListener('click', addDiv);
+main.addEventListener('click', addDiv);
 
 
 function close (e) {
     e.path[1].children[1].style.display = 'block';
+
     let kub = e.path[1];
-    let my = kub.getBoundingClientRect();
 
-    kub.onmousedown = function(e) {
+    let mainDiv = main.getBoundingClientRect();
 
-        let coordinates = getCoordinates(kub);
-        let mainDiv = div.getBoundingClientRect();
-        let shiftX = e.pageX - coordinates.left;
-        let shiftY = e.pageY - coordinates.top;
-
-        kub.style.position = 'absolute';
-        document.body.appendChild(kub);
+    kub.onmousedown = function () {
         moveAt(e);
 
-        kub.style.zIndex = 1000;
+            function moveAt(e) {
 
-        function moveAt(e) {
-            if(mainDiv.right > e.pageX && mainDiv.left < e.pageX && mainDiv.top < e.pageY && mainDiv.bottom > e.pageY){
-                kub.style.left = e.pageX - shiftX + 'px';
-                kub.style.top = e.pageY - shiftY + 'px';
+                if(e.x > mainDiv.left && e.x < mainDiv.right){
+                    kub.style.left = e.pageX - 240 - kub.offsetWidth / 2 + 'px';
+                }
+                if(e.y < mainDiv.bottom && e.y > mainDiv.top){
+                    kub.style.top = e.pageY - 50 - kub.offsetHeight / 2 + 'px';
+                }
             }
 
-        }
+            document.onmousemove = function(e) {
+                moveAt(e);
+            };
 
-        document.onmousemove = function(e) {
-            moveAt(e);
-        };
-
-        kub.onmouseup = function() {
-            document.onmousemove = null;
-            kub.onmouseup = null;
-        };
-
-    }
-
-}
-
-function getCoordinates(elem) {
-    let box = elem.getBoundingClientRect();
-    return {
-        top: box.top + pageYOffset,
-        left: box.left + pageXOffset
+            document.onmouseup = function() {
+                document.onmousemove = null;
+                kub.onmouseup = null;
+            };
     };
 }
 
